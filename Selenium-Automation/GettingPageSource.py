71654713525b68ab8213bb0b3fc6f56696b9d57a
Joel Wembo <joelotepawembo@gmail.com>
2021-05-11 17:43:58 +0800

    Creating web scraping

--- Selenium-Automation/GettingPageSource.py
+++ Selenium-Automation/GettingPageSource.py
@@ -1,15 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import time
 
 options = Options()
 options.headless = True
 options.add_argument("--window-size=1920,1200")
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(options=options, executable_path=PATH)
-driver.get("https://www.joelotepawembo.com/")
+driver.get("https://www.google.com/")
 data = driver.page_source
 print(data)
-f= open("cnn.html","w+")
+f= open("test.html","w+")
 f.write(data)
+time.sleep(1)
+
+driver.save_screenshot('screenshot.png')
 
 driver.quit()
