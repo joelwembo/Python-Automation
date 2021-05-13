3070ba4e5837150f6dd975e77affed2b65a3c69b
Joel Wembo <joelotepawembo@gmail.com>
2021-05-13 19:34:49 +0800

    Pyttest example

---
+++ QATesting/PytestTesting/test1_example1.py
@@ -0,0 +1,25 @@
+# Write a function that takes,,,
+
+def vowel_count(string):
+    vowels =['p', 'w','f','a', 'i', 't', 'e']
+    count = 0
+    for ch in string: 
+        if ch in vowels: 
+            count += 1
+    return count
+
+#print(vowel_count('otepa'))
+#print(vowel_count('wembo'))
+#print(vowel_count('fabrice'))
+
+def test_with_f_name():
+    assert vowel_count('otepa') == 4
+
+def test_with_l_name():
+    assert vowel_count('wembo') == 2
+
+def test_with_c_name():
+    assert vowel_count('fabrice') == 4     
+
+def test_with_c_name():
+    assert vowel_count('joel wembo') == 3   
