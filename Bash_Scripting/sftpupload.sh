2fa1172f992c4ec20fbb95ac85900c16bf7c0429
Joel Wembo <joelotepawembo@gmail.com>
2021-07-11 20:28:36 +0800

    Create sftpupload.sh

---
+++ Bash_Scripting/sftpupload.sh
@@ -0,0 +1,9 @@
+sftp -v -oIdentityFile=path user@server <<EOF
+put localPath ftpPath
+EOF
+
+local_path=/path/to/local/file
+remote_path=/somewhere/or/other
+sftp -v -oIdentityFile=path user@server <<EOF
+put $local_path $remote_path
+EOF
