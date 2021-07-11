4d08da2df85d9ba9aa92cab65d9121fe86a2e1e0
Joel Wembo <joelotepawembo@gmail.com>
2021-07-11 20:35:08 +0800

    Create config.py

---
+++ Bash_Scripting/pythonftp/config.py
@@ -0,0 +1,10 @@
+# SFTP server IP, port, account, password
+host = "172.16.53.113"
+port = 22  # not str
+username = "admin"
+password = "123456"
+ 
+ # remote and local are relative client
+homeDir = "D:/SFTP/"
+localDir = "E:/SFTP/"
+ 
