c98ab7d4884bfa37e0135d1ca35be88fad05dbb9
Joel Wembo <joelotepawembo@gmail.com>
2021-07-31 18:49:36 +0800

    Update autoWebsearch.py

--- Selenium-Automation/autoWebsearch.py
+++ Selenium-Automation/autoWebsearch.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 PATH = "C:\Program Files (x86)\chromedriver.exe"
-
+PATH_2 = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
 driver.get("https://techwithtim.net")
