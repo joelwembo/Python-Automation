b479212822bd2ed5153b3e1b6e62202eaca12462
Joel Wembo <joelotepawembo@gmail.com>
2021-08-25 22:43:26 +0800

    Update sftpupload.sh

--- Bash_Scripting/sftpupload.sh
+++ Bash_Scripting/sftpupload.sh
@@ -3,6 +3,7 @@ put localPath ftpPath
 EOF
 
 local_path=/path/to/local/file
+remote_path=/path/to/local/file
 remote_path=/somewhere/or/other
 sftp -v -oIdentityFile=path user@server <<EOF
 put $local_path $remote_path
