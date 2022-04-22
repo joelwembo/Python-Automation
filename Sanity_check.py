ea6d1f0ed55772fdbd91fa2365a16a52728a1b90
Joel Wembo <joelotepawembo@gmail.com>
2022-04-22 21:06:52 +0800

    Update Sanity_check.py

--- Sanity_check.py
+++ Sanity_check.py
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+# steps to be undertaken to configure git repository so that it runs the code sanity checking tooks before any commits
+# prevent it from happening again if the sanity testing fails
+
 files=$(git diff –cached –name-only –diff-filter=ACM | grep ‘.py$’)
 if [ -z files ]; then
 exit 0
